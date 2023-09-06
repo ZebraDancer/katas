2131ca3e3052ce0a4d81d16fc2c3f5384be3f198
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-09-06 13:17:52 +0200

    --no-edit

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-function validatePIN(pin) {
-  return /^(\d{4}|\d{6})$/.test(pin);
-}
+const saleHotdogs = (n) => {
+  if (n < 5) {
+    return n * 100;
+  } else if (n >= 5 && n < 10) {
+    return n * 95;
+  } else {
+    return n * 90;
+  }
+};
 
-validatePIN("0000");
+saleHotdogs(10);
 
-//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
-//If the function is passed a valid PIN string, return true, else return false.
+//Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+
+//number of hotdogs	price per unit (cents)
+//n < 5	100
+//n >= 5 and n < 10	95
+//n >= 10	90
+
+//function saleHotdogs(n){
+//return n*(n<5?100:n<10?95:90);}
