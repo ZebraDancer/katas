fef90776abfdd80c240eb3f6a19d8f5406e13111
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-09-26 13:10:53 +0200

    find indexOf

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const solution = (str, end) => str.endsWith(end);
+const findNeedle = (array) => {
+  let position = array.indexOf('needle');
+  return `Found the needle at position ${position}`;
+};
 
-solution('abcde', 'cde');
+let haystack_2 = [
+  '283497238987234',
+  'a dog',
+  'a cat',
+  'some random junk',
+  'a piece of hay',
+  'needle',
+  'something somebody lost a while ago',
+];
+
+findNeedle(haystack_2);
