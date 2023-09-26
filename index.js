d41fa459b9241f9370ffb52b4fbecad56d1542c4
VANESSA MARQUES <vanessa.m.a1989@gmail.com>
2023-09-26 20:10:40 +0200

    Convert number to reversed array of digits

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-const findNeedle = (array) => {
-  let position = array.indexOf('needle');
-  return `Found the needle at position ${position}`;
-};
+const digitize = (n) =>
+  n
+    .toString()
+    .split('')
+    .reverse()
+    .map((n) => parseFloat(n));
 
-let haystack_2 = [
-  '283497238987234',
-  'a dog',
-  'a cat',
-  'some random junk',
-  'a piece of hay',
-  'needle',
-  'something somebody lost a while ago',
-];
-
-findNeedle(haystack_2);
+digitize(2635353);
