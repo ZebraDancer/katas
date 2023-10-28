86a9669b7a0af1adaa72080f93451e591a8ff919
Vanessa Marques <vanessa.m.a1989@gmail.com>
2023-10-28 19:30:23 +0200

    convert km/h to cm/s

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const alphabetPosition = (str) => {
-
-return str.toLowerCase().split('').map(char => char.charCodeAt(0) -96).filter(char => char > 0 && char < 27).join(' ')
-
-
+const cockroachSpeed = (s) => {
+  return Math.floor((s * 100000) / 3600);
 };
 
+cockroachSpeed(1.08);
 
-alphabetPosition("The sunset sets at twelve o' clock.");
+//108,000 cm / 3600
+//1 km is equal to 100,000 cm. 1 hour is equal to 3600 sec. So converted version is 7,200,000 cm / 3600 sec. = 2000 cm / sec.
