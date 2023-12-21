50c5e74f7fc286adb10a758dff6de9fd60a594cb
ZebraDancer <vanessamarques@Lisbonita.local>
2023-12-21 11:15:45 +0100

    sort odd number in array, keep even at the same index

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
-const order = (words) => {
-  if(words.length === 0) {
-    return ""
-  }
-  
-  const array = words.split(" ")  
-  
-  const arrayRange = (start, stop, step) =>
-      Array.from(
-      { length: (stop - start) / step + 1 },
-      (value, index) => start + index * step
-      );
-  
-  const range = arrayRange(1, 9, 1)
-  
-  let finalSentence = []
-  
-  for (const n of range) {
-      for (const word of array) {
-        if(word.includes(n)) {
-          finalSentence.push(word)
-        }
-     }
-  }
+const sortArray = (array) => {
+const arr =  array.sort((a, b) => a%2 && b%2 ? a - b : null)
+return arr
+}
 
-  //test
-  
-  return finalSentence.join(" ")
-  
-  }
+sortArray([5, 3, 1, 8, 0])
